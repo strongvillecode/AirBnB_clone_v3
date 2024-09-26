de95c9380560f96229d31066158bc1b805d9c63e
strongvillecode <107783160+strongvillecode@users.noreply.github.com>
2024-09-26 17:58:12 +0100

    airbnb 3

---
+++ models/user.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" holds class User"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import hashlib
+
+
+class User(BaseModel, Base):
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        __password = Column("password", String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", cascade="all, delete", backref="user")
+        reviews = relationship("Review", cascade="all, delete", backref="user")
+    else:
+        email = ""
+        __password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+
+    @property
+    def password(self):
+        """password getter"""
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        """hash and set value"""
+        m = hashlib.new('md5')
+        m.update(value.encode('utf-8'))
+        self.__password = str(m.hexdigest())
---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python3
+"""
+Contains the TestDBStorageDocs and TestDBStorage classes
+"""
+
+from datetime import datetime
+import inspect
+import models
+from models.engine import db_storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+import json
+import os
+import pep8
+import unittest
+DBStorage = db_storage.DBStorage
+classes = {"Amenity": Amenity, "City": City, "Place": Place,
+           "Review": Review, "State": State, "User": User}
+
+
+class TestDBStorageDocs(unittest.TestCase):
+    """Tests to check the documentation and style of DBStorage class"""
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the doc tests"""
+        cls.dbs_f = inspect.getmembers(DBStorage, inspect.isfunction)
+
+    def test_pep8_conformance_db_storage(self):
+        """Test that models/engine/db_storage.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['models/engine/db_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_conformance_test_db_storage(self):
+        """Test tests/test_models/test_db_storage.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_models/test_engine/\
+test_db_storage.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_db_storage_module_docstring(self):
+        """Test for the db_storage.py module docstring"""
+        self.assertIsNot(db_storage.__doc__, None,
+                         "db_storage.py needs a docstring")
+        self.assertTrue(len(db_storage.__doc__) >= 1,
+                        "db_storage.py needs a docstring")
+
+    def test_db_storage_class_docstring(self):
+        """Test for the DBStorage class docstring"""
+        self.assertIsNot(DBStorage.__doc__, None,
+                         "DBStorage class needs a docstring")
+        self.assertTrue(len(DBStorage.__doc__) >= 1,
+                        "DBStorage class needs a docstring")
+
+    def test_dbs_func_docstrings(self):
+        """Test for the presence of docstrings in DBStorage methods"""
+        for func in self.dbs_f:
+            self.assertIsNot(func[1].__doc__, None,
+                             "{:s} method needs a docstring".format(func[0]))
+            self.assertTrue(len(func[1].__doc__) >= 1,
+                            "{:s} method needs a docstring".format(func[0]))
+
+
+class TestFileStorage(unittest.TestCase):
+    """Test the FileStorage class"""
+    def tearDown(self):
+        """commit session changes"""
+        models.storage._DBStorage__session.commit()
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_all_returns_dict(self):
+        """Test that all returns a dictionaty"""
+        self.assertIs(type(models.storage.all()), dict)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_all_no_class(self):
+        """Test that all returns all rows when no class is passed"""
+        state_inst = State(name="California")
+        amenity_inst = Amenity(name="wifi")
+        models.storage._DBStorage__session.add(state_inst)
+        models.storage._DBStorage__session.add(amenity_inst)
+        models.storage._DBStorage__session.commit()
+        self.assertEqual(len(models.storage.all()), 2)
+        models.storage._DBStorage__session.delete(state_inst)
+        models.storage._DBStorage__session.delete(amenity_inst)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_new(self):
+        """test that new adds an object to the database"""
+        state_inst = State(name="California")
+        models.storage.new(state_inst)
+        models.storage._DBStorage__session.commit()
+        self.assertEqual(len(models.storage.all()), 1)
+        models.storage._DBStorage__session.delete(state_inst)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_save(self):
+        """Test that save properly saves objects to database"""
+        state_inst = State(name="California")
+        models.storage._DBStorage__session.add(state_inst)
+        models.storage._DBStorage__session.commit()
+        self.assertEqual(len(models.storage.all()), 1)
+        models.storage._DBStorage__session.delete(state_inst)
+        models.storage.save()
+        self.assertEqual(len(models.storage.all()), 0)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get(self):
+        """Test that get properly gets an object from database"""
+        state_inst = State(name="California")
+        models.storage._DBStorage__session.add(state_inst)
+        models.storage._DBStorage__session.commit()
+        self.assertIs(models.storage.get("State", state_inst.id), state_inst)
+        self.assertIs(models.storage.get("State", "wrong id"), None)
+        self.assertIs(models.storage.get("some class", state_inst.id), None)
+        models.storage._DBStorage__session.delete(state_inst)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        """Test that count properly counts an objects from database"""
+        state_inst = State(name="California")
+        amenity_inst = Amenity(name="wifi")
+        models.storage._DBStorage__session.add(state_inst)
+        models.storage._DBStorage__session.add(amenity_inst)
+        models.storage._DBStorage__session.commit()
+        self.assertEqual(models.storage.count(), 2)
+        self.assertEqual(models.storage.count("State"), 1)
+        self.assertEqual(models.storage.count("Amenity"), 1)
+        self.assertEqual(models.storage.count("City"), 0)
+        models.storage._DBStorage__session.delete(state_inst)
+        models.storage._DBStorage__session.delete(amenity_inst)
